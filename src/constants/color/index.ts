989fda0f307f0e48715101f92b304e1b72fcf7f8
datha <dath33603@gmail.com>
2023-11-21 06:31:25 +0700

    feat-add-constant-color-and-fake-data

---
+++ src/constants/color/index.ts
@@ -0,0 +1,9 @@
+export enum COLORS {
+  WHITE = '#FFFFFF',
+  GREY = '#CBCBD4',
+  BLACK = '#000',
+  BROWN = '#846046',
+  GREY_1 = '#CBCBD4',
+  WHITE_1 = '#F8F7FA',
+  TRANSPARENT = 'transparent',
+}
---
+++ src/constants/fakeData/index.ts
@@ -0,0 +1,45 @@
+// page splash screen
+export const pages = [
+  {
+    image:
+      'https://i.pinimg.com/originals/bf/24/47/bf24476728abedad530e71fced5c7e51.jpg',
+    title: ['Good Coffee 1 ', 'Good Friends 1', 'let it blends 1'],
+    description: 'The best grain, the finest roast, the most powerful flavor. ',
+  },
+  {
+    image:
+      'https://media.istockphoto.com/id/1177900338/vi/anh/t%C3%A1ch-c%C3%A0-ph%C3%AA-espresso-v%E1%BB%9Bi-h%E1%BA%A1t-c%C3%A0-ph%C3%AA.jpg?s=612x612&w=0&k=20&c=cSiz3e-XL6bU60GdsZ-N1eEZkvEtCkQzZ5dZJbthM68=',
+    title: ['Good Coffee 2 ', 'Good Friends 2', 'let it blends 2'],
+
+    description: 'The best grain, the finest roast, the most powerful flavor. ',
+  },
+  {
+    image:
+      'https://media.istockphoto.com/id/1271386167/vi/anh/c%C3%A0-ph%C3%AA.jpg?s=612x612&w=0&k=20&c=Q3ok85emjnUTMd7PIMZiMSRh1J853we5A9UGVvpuL4w=',
+    title: ['Good Coffee 3 ', 'Good Friends 3', 'let it blends 3'],
+    description: 'The best grain, the finest roast, the most powerful flavor.',
+  },
+];
+
+// category
+import CappuccinoSvg from '../../assets/img/cappuccino.svg';
+import ColdBrewSvg from '../../assets/img/coldbrew.svg';
+import ExpressoSvg from '../../assets/img/expresso.svg';
+
+export const categories = [
+  {
+    name: 'Cappuccino',
+    icon: CappuccinoSvg,
+    category: 'cappuccino',
+  },
+  {
+    name: 'Cold Brew',
+    icon: ColdBrewSvg,
+    category: 'coldbrew',
+  },
+  {
+    name: 'Expresso',
+    icon: ExpressoSvg,
+    category: 'expresso',
+  },
+];
