102fabb0e70abdace46ea5a38cf717d6840daf39
datha <dath33603@gmail.com>
2023-11-21 06:14:22 +0700

    change-move tab1,2,3 in main folder

---
+++ src/screen/main/tab2/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+const Tab2 = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Tab2</Text>
+    </View>
+  );
+};
+export default Tab2;
