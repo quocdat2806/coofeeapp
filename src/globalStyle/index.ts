ba41894a8e920268f195872c600d8463c79bbb5f
datha <dath33603@gmail.com>
2023-11-23 14:36:23 +0700

    feat : add global style

---
+++ src/globalStyle/index.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const globalStyle = StyleSheet.create({
+  'd-flex': {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
+
+export default globalStyle;
